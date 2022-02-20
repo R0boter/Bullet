1175a36ba4da55fb0634464057a12c5e438fb2e2
Leon <leonsout@outlook.com>
2022-02-20 12:32:20 +0800

    添加静态服务器

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const { port } = require('./config');
 const koaBody = require('koa-body');
+const static = require('koa-static');
 const cors = require('@koa/cors');
 const router = require('./router');
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(koaBody());
 
 app.use(router.routes()).use(router.allowedMethods());
+app.use(static(__dirname + '/public/'));
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
