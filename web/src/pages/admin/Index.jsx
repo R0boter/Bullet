d99c1258b085061a2f7218eeab4fe79bc01d1df3
Falcon <leonsout@outlook.com>
2022-04-07 14:10:19 +0800

    后台页面自动跳转

--- web/src/pages/admin/Index.jsx
+++ web/src/pages/admin/Index.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 
 const Index = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (pathname === '/admin' || pathname === '/admin/') {
+      navigate('/admin/home');
+    }
+  }, []);
+
   return (
     <>
       <Outlet />
